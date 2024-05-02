c9c66bc0baaad3c6cf78fd653323a4efe8f4666b
Saidkamol Mirsaidov <kamolsaid2006@gmail.com>
2024-05-02 17:15:39 +0500

    add ExecutorService to Main.clas

--- src/main/java/uz/pdp/ui/Main.java
+++ src/main/java/uz/pdp/ui/Main.java
@@ -5,23 +5,27 @@ import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
 import uz.pdp.ui.handlers.BotHandler;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
     public static final String TOKEN = "6789038344:AAHbE6duttOiyH7W6u0MVbYQgtzjpbNsrfk";
 
     public static final TelegramBot BOT = new TelegramBot(TOKEN);
 
     public static final ThreadLocal<BotHandler> BOT_HANDLER_THREAD_LOCAL = ThreadLocal.withInitial(BotHandler::new);
+    public static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public static void main(String[] args) {
         BOT.setUpdatesListener(list -> {
-            try {
-                for (Update update : list) {
+            for (Update update : list) {
+                CompletableFuture.runAsync(() -> {
                     BOT_HANDLER_THREAD_LOCAL.get().handle(update);
-                }
-            } finally {
-                return UpdatesListener.CONFIRMED_UPDATES_ALL;
-            }
+                }, EXECUTOR_SERVICE);
 
+            }
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
         });
     }
 }
